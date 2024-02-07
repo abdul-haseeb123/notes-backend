4de3eab995b4ce2101c630534fe5ef7385e4ec16
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-02-07 22:14:40 +0500

    first-commit

---
+++ src/api/mcq/controllers/mcq.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mcq controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mcq.mcq');
---
+++ src/api/mcq/services/mcq.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mcq service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mcq.mcq');
